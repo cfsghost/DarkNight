605933e469a58c1b7fc21fe63b7662e349ffecab
Fred Chien <cfsghost@gmail.com>
2015-05-01 07:28:47 +0800

    Fixed a bug incorrect bootstrap component used

--- src/libs/components/admin/Member-AwardsManager.jsx
+++ src/libs/components/admin/Member-AwardsManager.jsx
@@ -2,7 +2,8 @@
 
 var React = require('react');
 var Bootstrap = require('react-bootstrap');
-var Grids = Bootstrap.Grids;
+var Glyphicon = Bootstrap.Glyphicon;
+var Grid = Bootstrap.Grid;
 var Col = Bootstrap.Col;
 var Row = Bootstrap.Row;
 var MemberStore = require('../../stores/Member');
@@ -14,7 +15,7 @@ var Item = React.createClass({
 	console.log(this.props.award);
 
 		return (
-			<Col sm={6} md={4}>
+			<Col xs={6} md={4}>
 				<div className='thumbnail'>
 					<img src={'/award/' + this.props.award.icon + '/icon'} />
 				</div>
@@ -23,7 +24,7 @@ var Item = React.createClass({
 	}
 });
 
-var AwardsManager = React.createClass({
+var MemberAwardsManager = React.createClass({
 	getInitialState: function() {
 		return {
 			awards: []
@@ -36,16 +37,16 @@ var AwardsManager = React.createClass({
 	},
 	componentDidMount: function() {
 		MemberStore.on('AwardsUpdated', this._update);
-console.log(this.props.memberId);
+
 		// Fetching member list 
-//		MemberActions.FetchAwards(this.props.memberId);
+		MemberActions.FetchAwards(this.props.memberId);
 	},
 	componentWillUnmount: function() {
 		MemberStore.removeListener('AwardsUpdated', this._update);
 	},
 	componentWillReceiveProps: function(nextProps) {
 console.log(nextProps);
-//		MemberActions.FetchAwards(nextProps.memberId);
+		MemberActions.FetchAwards(nextProps.memberId);
 	},
 	render: function() {
 		var items = [];
@@ -53,22 +54,29 @@ console.log(nextProps);
 		for (var id in this.state.awards) {
 			items.push(<Item award={this.state.awards[id]}/>);
 		}
-console.log(123123123);
+
+		items.push(
+			<Col sm={6} xs={2} md={4}>
+				<div className='thumbnail'>
+					<Glyphicon glyph='plus' />
+				</div>
+			</Col>
+		);
+
 		return (
-			<Grids>
+			<Grid>
 				<Row>
+					{items}
 				</Row>
-			</Grids>
+			</Grid>
 		);
 	},
 	_update: function(awards) {
 
-console.log(123123123);
 		this.setState({
 			awards: awards
 		});
-//		this.forceUpdate();
 	}
 });
 
-module.exports = AwardsManager;
+module.exports = MemberAwardsManager;
