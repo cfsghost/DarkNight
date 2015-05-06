ecf59fc7ec298f86e2face08ddad245f02083e91
leonlin14 <leonlin14@gmail.com>
2015-05-06 22:27:27 +0800

    add test template

---
+++ public/views/LoginApp.js
@@ -0,0 +1,8 @@
+var app = angular.module('DarkNight', []);
+
+app.controller('LoginController', function ($scope, $rootScope, $http) {
+    $http.get('/members/', {}).success(function(resp) {
+        console.log(resp)
+        $scope.test = resp.pageCount;
+    });
+});
