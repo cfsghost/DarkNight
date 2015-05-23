18eedfad18f3f98497cd15f9a9de3d87e1159c26
Rukeith <fenglan.crazydance@gmail.com>
2015-05-24 02:17:26 +0800

    add authenticate function to access /admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,12 @@ var Router = require('koa-router');
 
 var router = module.exports = new Router();
 
-router.get('/admin', function *() {
+router.get('/admin', function *(next) {
+	// add authenticate function to avoid anyone access admin
     yield this.render('admin');
+    if (this.isAuthenticated()) {
+	    yield this.render('admin');
+	} else {
+		this.redirect('/')
+	}
 });
